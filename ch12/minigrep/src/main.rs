bb720b86bab7383df8eea0eee8e614fce9d760f3
t_ishikawa <tissi0708@gmail.com>
2022-11-10 09:11:50 +0900

    add reading file part

--- ch12/minigrep/src/main.rs
+++ ch12/minigrep/src/main.rs
@@ -1,4 +1,6 @@
 use std::env;
+use std::fs::File;
+use std::io::Read;
 
 fn main() {
     println!("ch12-1 starts!!");
@@ -11,5 +13,14 @@ fn main() {
 
     println!("Searching for {}", query);
     println!("In file {}", filename);
+
+    let mut f = File::open(filename).expect("file not found");
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        .expect("something went wrong reading the file");
+
+    println!("With text:\n{}", contents);
+
 
 }
