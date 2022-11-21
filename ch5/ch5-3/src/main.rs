1c4abbc85736384aa657a5dd33334ceb0db8982b
t_ishikawa <tissi0708@gmail.com>
2022-11-21 16:43:24 +0900

    add a process calling a static method.

--- ch5/ch5-3/src/main.rs
+++ ch5/ch5-3/src/main.rs
@@ -34,4 +34,7 @@ fn main() {
 
     println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
     println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    let square = Rectangle::square(200);
+    println!("{}", square.area());
 }
