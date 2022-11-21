7ed936b27a4d520cdccfdfea98e70230151a4eff
t_ishikawa <tissi0708@gmail.com>
2022-11-21 18:04:04 +0900

    add search function

--- ch12/minigrep/src/lib.rs
+++ ch12/minigrep/src/lib.rs
@@ -25,11 +25,52 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let mut f = File::open(config.filename)?;
 
     let mut contents = String::new();
-    f.read_to_string(&mut contents)
-        .expect("something went wrong reading the file");
+    f.read_to_string(&mut contents)?;
 
-    println!("\nWith text:\n{}", contents);
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
 
     Ok(())
 }
 
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            // do something with line
+            results.push(line)
+        }
+    }
+
+    results
+}
+
+// pub fn search2(query: &str, contents: &str) -> Vec<&str> {
+//     vec![]
+// }
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search(query, contents)
+        );
+
+        // assert_eq!(
+        //     vec!["safe, fast, productive."],
+        //     search2(query, contents)
+        // );
+    }
+}
