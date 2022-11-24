591c58d81b73e58f2e812262e2a9a1c0e0276459
t_ishikawa <tissi0708@gmail.com>
2022-11-25 02:31:30 +0900

    update to  print errors to standard error

--- ch12/minigrep/src/main.rs
+++ ch12/minigrep/src/main.rs
@@ -10,15 +10,14 @@ fn main() {
     println!("ch12-1 starts!!");
 
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 
